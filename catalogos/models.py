4689904d88162fa0ae8692f8c3adcc47a86342af
cleomarlisboa <cleomar.srv@gmail.com>
2023-04-24 10:29:15 -0300

    implementacao, commit-001

---
+++ catalogos/models.py
@@ -0,0 +1,90 @@
+from django.db import models
+from django.urls import reverse
+import uuid
+
+class meuModelo(models.Model):
+
+    meuCampo = models.CharField(max_length=20, help_text='digite meuCampo')
+
+    class Meta:
+        ordering = ['-meuCampo']
+
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.id)])
+
+    def __str__(self):
+        return self.my_field_name
+
+class Genero(models.Model):
+    nome = models.CharField(max_length=200, help_text='digite um genero de livro')
+
+    def __str__(self):
+        return self.nome
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=200)
+
+    autor = models.ForeignKey('Autor', on_delete=models.SET_NULL, null=True)
+
+    sumario = models.TextField(max_length=1000, help_text='digite uma breve descrição do livro')
+    isbn = models.CharField('ISBN', max_length=13, help_text='13 caracteres <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+
+    genero = models.ManyToManyField(Genero, help_text='selecione o(s) genero(s) do livro')
+    idioma = models.ForeignKey('Idioma', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.titulo
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
+    
+
+class LivroFisico(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='ID exclusivo para este livro específico.')
+    livro = models.ForeignKey('Livro', on_delete=models.SET_NULL, null=True)
+    imprimir = models.CharField(max_length=200)
+    dataDevolucao= models.DateField(null=True, blank=True)
+
+    SituacaoEmprestimo = (
+        ('m', 'Manutenção'),
+        ('o', 'Emprestado'),
+        ('a', 'Disponivel'),
+        ('r', 'Reservado'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=SituacaoEmprestimo,
+        blank=True,
+        default='m',
+        help_text='Disponibilidade do livro:',
+    )
+
+    class Meta:
+        ordering = ['dataDevolucao']
+
+    def __str__(self):
+        return f'{self.id} ({self.livro.titulo})'
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100)
+    dataNascimento = models.DateField(null=True, blank=True)
+    DataMorte = models.DateField('Morto', null=True, blank=True)
+
+    class Meta:
+        ordering = ['nome', 'sobrenome']
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.nome}, {self.sobrenome}'
+
+class Idioma(models.Model):
+    nome = models.CharField(max_length=200,
+                            help_text="digite o idioma do livro")
+
+    def __str__(self):
+        return self.nome
+    
---
+++ django_Biblioteca/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('catalogos/', include('catalogos.urls')),
+]
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalogos/')),
+]
